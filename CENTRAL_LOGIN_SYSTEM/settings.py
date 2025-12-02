24e6238611cf6004ab269c9e2b8512f9926987ea
sajith231 <sajiththomas231@gmail.com>
2025-12-02 11:59:58 +0530

    ui updations

--- CENTRAL_LOGIN_SYSTEM/settings.py
+++ CENTRAL_LOGIN_SYSTEM/settings.py
@@ -12,7 +12,7 @@ load_dotenv()
 
 # -------------------- Core ---------------------
 SECRET_KEY = 'django-insecure-c_%uwi0@g9fzwpollp#n1i3q-a=h+_4c(!z51fnj1ljm(zyg$$'
-DEBUG = True
+DEBUG = False
 ALLOWED_HOSTS: list[str] = ['activate.imcbs.com','www.activate.imcbs.com',"*"]
 
 # -------------------- Apps ---------------------
@@ -114,7 +114,7 @@ DATABASES = {
         'ENGINE': 'django.db.backends.postgresql',
         'NAME': 'central_login_system',  # Your DB name
         'USER': 'postgres',           # Your DB username
-        'PASSWORD': 'sajith@2003',   # Your DB password
+        'PASSWORD': 'info@imc',   # Your DB password
         'HOST': 'localhost',          # Or server IP if remote
         'PORT': '5432',               # Default PostgreSQL port
     }
