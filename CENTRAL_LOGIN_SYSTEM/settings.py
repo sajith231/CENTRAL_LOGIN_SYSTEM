d9ab7ca736980e44d213a4ea9c0a8a40b8b504dc
sajith231 <sajiththomas231@gmail.com>
2026-01-30 12:27:44 +0530

    demolicencing page setup complited

--- CENTRAL_LOGIN_SYSTEM/settings.py
+++ CENTRAL_LOGIN_SYSTEM/settings.py
@@ -12,7 +12,7 @@ load_dotenv()
 
 # -------------------- Core ---------------------
 SECRET_KEY = 'django-insecure-c_%uwi0@g9fzwpollp#n1i3q-a=h+_4c(!z51fnj1ljm(zyg$$'
-DEBUG = True
+DEBUG = False
 ALLOWED_HOSTS: list[str] = ['activate.imcbs.com','www.activate.imcbs.com',"*"]
 
 # -------------------- Apps ---------------------
