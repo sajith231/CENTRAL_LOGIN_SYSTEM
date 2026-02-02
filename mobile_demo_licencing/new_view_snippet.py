d67417c3d19e43f7bde07ec66dc126a26f50c66d
sajith231 <sajiththomas231@gmail.com>
2026-02-02 10:14:57 +0530

    demo licencing complited

--- mobile_demo_licencing/new_view_snippet.py
+++ mobile_demo_licencing/new_view_snippet.py
@@ -1,4 +1,11 @@
 
+from pyexpat.errors import messages
+from django.shortcuts import get_object_or_404
+from flask import redirect
+
+from MobileApp.models import ActiveDevice
+
+
 def delete_demo_device(request, pk):
     device = get_object_or_404(ActiveDevice, pk=pk)
     device.delete()
